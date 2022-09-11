81d3e1a4acf56625a26bae9278b7731cf5dbbe17
“saurabh-paystack” <saurabh@paystack.com>
2022-09-12 00:11:22 +0400

    fix tyo

--- services/knote/remote.tf
+++ services/knote/remote.tf
@@ -8,12 +8,12 @@ data "terraform_remote_state" "setup" {
   }
 }
 
-# data "terraform_remote_state" "k8s" {
-#   backend = "s3"
-#   config = {
-#     bucket  = "zilliqa-devops-dev-state"
-#     key     = "k8s/terraform.tfstate"
-#     region  = "eu-west-1"
-#     profile = "dev-zilliqa-infra"
-#   }
-# }
+data "terraform_remote_state" "k8s" {
+  backend = "s3"
+  config = {
+    bucket  = "zilliqa-devops-dev-state"
+    key     = "k8s/terraform.tfstate"
+    region  = "eu-west-1"
+    profile = "dev-zilliqa-infra"
+  }
+}
