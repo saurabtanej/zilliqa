70a85424f27fefe0ab2a2c63261cfb9ff10ecd68
“saurabh-paystack” <saurabh@paystack.com>
2022-09-11 23:59:57 +0400

    fic typo

--- setup/networking.tf
+++ setup/networking.tf
@@ -19,7 +19,7 @@ module "vpc" {
   single_nat_gateway = true
   enable_nat_gateway = true
 
-  tags = tags = merge(local.common_tags, {
+  tags = merge(local.common_tags, {
     "k8s.io/cluster-autoscaler/${local.eks_cluster_name}" = "owned"
     "k8s.io/cluster-autoscaler/enabled"                   = "true"
   })
